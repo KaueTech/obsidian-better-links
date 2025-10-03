f3813370807642cb17768351aface3081147cee2
Kauê Dias <kauedias.dev@gmail.com>
2025-10-03 10:35:25 -0300

    first version

--- main.ts
+++ main.ts
@@ -1,134 +1,147 @@
-import { App, Editor, MarkdownView, Modal, Notice, Plugin, PluginSettingTab, Setting } from 'obsidian';
+import { Plugin, PluginSettingTab, Setting } from 'obsidian';
 
-// Remember to rename these classes and interfaces!
-
-interface MyPluginSettings {
-	mySetting: string;
+interface BetterLinksSettings {
+  internalFg: string;
+  internalFgHover: string;
+  internalUnderlineHover: string;
+  externalFg: string;
+  externalFgHover: string;
+  externalUnderlineHover: string;
+  underlineEnabled: boolean;
 }
 
-const DEFAULT_SETTINGS: MyPluginSettings = {
-	mySetting: 'default'
+const DEFAULT_SETTINGS: BetterLinksSettings = {
+  internalFg: 'rgb(0, 255, 255)',
+  internalFgHover: 'rgb(255, 255, 255)',
+  internalUnderlineHover: 'rgb(0, 148, 158)',
+  externalFg: 'rgb(255, 0, 221)',
+  externalFgHover: 'rgb(255, 255, 255)',
+  externalUnderlineHover: 'rgb(177, 0, 153)',
+  underlineEnabled: true,
+};
+
+export default class BetterLinks extends Plugin {
+  settings: BetterLinksSettings;
+
+  async onload() {
+    await this.loadSettings();
+    this.addSettingTab(new BetterLinksSettingTab(this.app, this));
+
+    // Aplica as variáveis CSS
+    this.applyCSSVars();
+  }
+
+  applyCSSVars() {
+    const root = document.documentElement;
+    root.style.setProperty('--better-internal-fg', this.settings.internalFg);
+    root.style.setProperty('--better-internal-fg-hover', this.settings.internalFgHover);
+    root.style.setProperty('--better-internal-underline-hover', this.settings.internalUnderlineHover);
+
+    root.style.setProperty('--better-external-fg', this.settings.externalFg);
+    root.style.setProperty('--better-external-fg-hover', this.settings.externalFgHover);
+    root.style.setProperty('--better-external-underline-hover', this.settings.externalUnderlineHover);
+
+    root.style.setProperty('--better-underline-enabled', this.settings.underlineEnabled ? '1' : '0');
+  }
+
+  async loadSettings() {
+    this.settings = Object.assign({}, DEFAULT_SETTINGS, await this.loadData());
+  }
+
+  async saveSettings() {
+    await this.saveData(this.settings);
+    this.applyCSSVars();
+  }
 }
 
-export default class MyPlugin extends Plugin {
-	settings: MyPluginSettings;
+class BetterLinksSettingTab extends PluginSettingTab {
+  plugin: BetterLinks;
 
-	async onload() {
-		await this.loadSettings();
+  constructor(app: any, plugin: BetterLinks) {
+    super(app, plugin);
+    this.plugin = plugin;
+  }
 
-		// This creates an icon in the left ribbon.
-		const ribbonIconEl = this.addRibbonIcon('dice', 'Sample Plugin', (_evt: MouseEvent) => {
-			// Called when the user clicks the icon.
-			new Notice('This is a notice!');
-		});
-		// Perform additional things with the ribbon
-		ribbonIconEl.addClass('my-plugin-ribbon-class');
+  display(): void {
+    const { containerEl } = this;
+    containerEl.empty();
 
-		// This adds a status bar item to the bottom of the app. Does not work on mobile apps.
-		const statusBarItemEl = this.addStatusBarItem();
-		statusBarItemEl.setText('Status Bar Text');
+    containerEl.createEl('h2', { text: 'Better Links Settings' });
 
-		// This adds a simple command that can be triggered anywhere
-		this.addCommand({
-			id: 'open-sample-modal-simple',
-			name: 'Open sample modal (simple)',
-			callback: () => {
-				new SampleModal(this.app).open();
-			}
-		});
-		// This adds an editor command that can perform some operation on the current editor instance
-		this.addCommand({
-			id: 'sample-editor-command',
-			name: 'Sample editor command',
-			editorCallback: (editor: Editor, _view: MarkdownView) => {
-				console.log(editor.getSelection());
-				editor.replaceSelection('Sample Editor Command');
-			}
-		});
-		// This adds a complex command that can check whether the current state of the app allows execution of the command
-		this.addCommand({
-			id: 'open-sample-modal-complex',
-			name: 'Open sample modal (complex)',
-			checkCallback: (checking: boolean) => {
-				// Conditions to check
-				const markdownView = this.app.workspace.getActiveViewOfType(MarkdownView);
-				if (markdownView) {
-					// If checking is true, we're simply "checking" if the command can be run.
-					// If checking is false, then we want to actually perform the operation.
-					if (!checking) {
-						new SampleModal(this.app).open();
-					}
+    new Setting(containerEl)
+      .setName('Internal Link Color')
+      .setDesc('Cor dos links internos')
+      .addText(text => text
+        .setPlaceholder('rgb(...) ou #hex')
+        .setValue(this.plugin.settings.internalFg)
+        .onChange(async (value) => {
+          this.plugin.settings.internalFg = value;
+          await this.plugin.saveSettings();
+        }));
 
-					// This command will only show up in Command Palette when the check function returns true
-					return true;
-				}
-			}
-		});
+    new Setting(containerEl)
+      .setName('Internal Link Hover Color')
+      .setDesc('Cor ao passar o mouse nos links internos')
+      .addText(text => text
+        .setPlaceholder('rgb(...) ou #hex')
+        .setValue(this.plugin.settings.internalFgHover)
+        .onChange(async (value) => {
+          this.plugin.settings.internalFgHover = value;
+          await this.plugin.saveSettings();
+        }));
 
-		// This adds a settings tab so the user can configure various aspects of the plugin
-		this.addSettingTab(new SampleSettingTab(this.app, this));
+    new Setting(containerEl)
+      .setName('Internal Link Underline Color (Hover)')
+      .setDesc('Cor do underline interno ao passar o mouse')
+      .addText(text => text
+        .setPlaceholder('rgb(...) ou #hex')
+        .setValue(this.plugin.settings.internalUnderlineHover)
+        .onChange(async (value) => {
+          this.plugin.settings.internalUnderlineHover = value;
+          await this.plugin.saveSettings();
+        }));
 
-		// If the plugin hooks up any global DOM events (on parts of the app that doesn't belong to this plugin)
-		// Using this function will automatically remove the event listener when this plugin is disabled.
-		this.registerDomEvent(document, 'click', (evt: MouseEvent) => {
-			console.log('click', evt);
-		});
+    new Setting(containerEl)
+      .setName('External Link Color')
+      .setDesc('Cor dos links externos')
+      .addText(text => text
+        .setPlaceholder('rgb(...) ou #hex')
+        .setValue(this.plugin.settings.externalFg)
+        .onChange(async (value) => {
+          this.plugin.settings.externalFg = value;
+          await this.plugin.saveSettings();
+        }));
 
-		// When registering intervals, this function will automatically clear the interval when the plugin is disabled.
-		this.registerInterval(window.setInterval(() => console.log('setInterval'), 5 * 60 * 1000));
-	}
+    new Setting(containerEl)
+      .setName('External Link Hover Color')
+      .setDesc('Cor ao passar o mouse nos links externos')
+      .addText(text => text
+        .setPlaceholder('rgb(...) ou #hex')
+        .setValue(this.plugin.settings.externalFgHover)
+        .onChange(async (value) => {
+          this.plugin.settings.externalFgHover = value;
+          await this.plugin.saveSettings();
+        }));
 
-	onunload() {
+    new Setting(containerEl)
+      .setName('External Link Underline Color (Hover)')
+      .setDesc('Cor do underline externo ao passar o mouse')
+      .addText(text => text
+        .setPlaceholder('rgb(...) ou #hex')
+        .setValue(this.plugin.settings.externalUnderlineHover)
+        .onChange(async (value) => {
+          this.plugin.settings.externalUnderlineHover = value;
+          await this.plugin.saveSettings();
+        }));
 
-	}
-
-	async loadSettings() {
-		this.settings = Object.assign({}, DEFAULT_SETTINGS, await this.loadData());
-	}
-
-	async saveSettings() {
-		await this.saveData(this.settings);
-	}
-}
-
-class SampleModal extends Modal {
-	constructor(app: App) {
-		super(app);
-	}
-
-	onOpen() {
-		const {contentEl} = this;
-		contentEl.setText('Woah!');
-	}
-
-	onClose() {
-		const {contentEl} = this;
-		contentEl.empty();
-	}
-}
-
-class SampleSettingTab extends PluginSettingTab {
-	plugin: MyPlugin;
-
-	constructor(app: App, plugin: MyPlugin) {
-		super(app, plugin);
-		this.plugin = plugin;
-	}
-
-	display(): void {
-		const {containerEl} = this;
-
-		containerEl.empty();
-
-		new Setting(containerEl)
-			.setName('Setting #1')
-			.setDesc('It\'s a secret')
-			.addText(text => text
-				.setPlaceholder('Enter your secret')
-				.setValue(this.plugin.settings.mySetting)
-				.onChange(async (value) => {
-					this.plugin.settings.mySetting = value;
-					await this.plugin.saveSettings();
-				}));
-	}
+    new Setting(containerEl)
+      .setName('Underline Enabled')
+      .setDesc('Se o underline animado deve aparecer')
+      .addToggle(toggle => toggle
+        .setValue(this.plugin.settings.underlineEnabled)
+        .onChange(async (value) => {
+          this.plugin.settings.underlineEnabled = value;
+          await this.plugin.saveSettings();
+        }));
+  }
 }
